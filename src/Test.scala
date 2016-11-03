bcdbbe8562ac58376ba45a1b58464c5f9910aadd
LI Haohui <herveli0918@gmail.com>
2016-11-03 11:19:44 +0100

    slick2d

---
+++ src/Test.scala
@@ -0,0 +1,23 @@
+
+
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
+import org.newdawn.slick.tiled.TiledMap;
+
+
+
+object Test {
+
+  def main(args: Array[String]) {
+    new AppGameContainer(new CharacterGame(), 800, 600, false)
+      .start()
+  }
+}
+
