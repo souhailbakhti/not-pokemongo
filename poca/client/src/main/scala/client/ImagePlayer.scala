626d8f5f505abbd1f95ce6687d0e996b3154cd9e
Youneslamghari <lamghari.younes@gmail.com>
2016-12-22 21:31:54 +0100

    image battles

---
+++ poca/client/src/main/scala/client/ImagePlayer.scala
@@ -0,0 +1,9 @@
+
+
+package client
+
+object ImagePlayer {
+  lazy val player = ImageUtils.createImage("character.png")
+  lazy val battleMain = ImageUtils.createImage("Graphics/Pictures/Battle.png")
+  lazy val battleFight = ImageUtils.createImage("Graphics/Pictures/Battle2.png")
+}
