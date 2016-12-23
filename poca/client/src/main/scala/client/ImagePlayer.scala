eb86058a242f7ac326a4b17733ac2b9f9e991c70
Youneslamghari <lamghari.younes@gmail.com>
2016-12-23 16:46:26 +0100

    start pic

--- poca/client/src/main/scala/client/ImagePlayer.scala
+++ poca/client/src/main/scala/client/ImagePlayer.scala
@@ -6,4 +6,5 @@ object ImagePlayer {
   lazy val player = ImageUtils.createImage("character.png")
   lazy val battleMain = ImageUtils.createImage("Graphics/Pictures/Battle.png")
   lazy val battleFight = ImageUtils.createImage("Graphics/Pictures/Battle2.png")
+  lazy val StartPicture = ImageUtils.createImage("Graphics/Pictures/Start.png")
 }
