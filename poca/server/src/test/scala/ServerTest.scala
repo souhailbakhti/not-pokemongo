9d2c95445226b187b975f19e6d7551d49bc4c247
SOUHAIL ALLAL AL BAKHTI <souhailbakhti@gmail.com>
2017-01-09 13:56:27 +0100

    Create a new class GameService.scala which contains the Akka-server Create GameAreaActor which uses a hashMap of players and then notify players if an event happened Add should register multiple players Test : this test capable to register multiple players in akka server and expect a list of players in every register event

--- poca/server/src/test/scala/ServerTest.scala
+++ poca/server/src/test/scala/ServerTest.scala
@@ -1,81 +1,42 @@
-
-import akka.actor.ActorSystem
-import akka.http.scaladsl.model.ws.{Message, TextMessage}
-import akka.http.scaladsl.server.{Directives}
 import akka.http.scaladsl.testkit.{ScalatestRouteTest, WSProbe}
-import akka.stream.{ActorMaterializer, FlowShape}
-import akka.stream.scaladsl.{Flow, GraphDSL, Merge}
 import org.scalatest.{FunSuite, Matchers}
+import server.GameService
 
-class ServerTest extends FunSuite with Matchers with ScalatestRouteTest{
-	test("should create a new Gameserver") {
+class ServerTest extends FunSuite with Matchers with ScalatestRouteTest {
 
-		new GameService()
-	}
-  test("should connect to the GameService websocket") {
-    // tests:
-    // create a testing probe representing the client-side
-    val gameService = new GameService()
-    val wsClient = WSProbe()
-
-    // WS creates a WebSocket request for testing
-    WS("/", wsClient.flow) ~> gameService.websocketRoute ~>
-      check {
-        // check response for WS Upgrade headers
-        isWebSocketUpgrade shouldEqual true
-      }
+  test("should create GameService") {
+    new GameService()
   }
-  test("should respond to a message"){
-    // tests:
-    // create a testing probe representing the client-side
-    val gameService = new GameService()
-    val wsClient = WSProbe()
 
-    // WS creates a WebSocket request for testing
-    WS("/", wsClient.flow) ~> gameService.websocketRoute ~>
-      check {
-        // check response for WS Upgrade headers
-        wsClient.expectMessage("Welcome to Pokemon-go player!")
-        wsClient.sendMessage("Hello PLayer!")
-        wsClient.expectMessage("Hello PLayer!")
-      }
-
+  test("should be able to connect to the GameService websocket") {
+    assertWebsocket("John") { wsClient =>
+      isWebSocketUpgrade shouldEqual true
+    }
   }
-  test("should subscribe a player"){
-    // tests:
-    // create a testing probe representing the client-side
+
+  test("should register player") {
+    assertWebsocket("John"){ wsClient =>
+      wsClient.expectMessage("[{\"name\":\"John\",\"position\":{\"x\":0,\"y\":0}}]")
+    }
+  }
+
+  test("should register multiple players") {
+    val gameService = new GameService()
+    val johnClient = WSProbe()
+    val andrewClient = WSProbe()
+
+    WS(s"/?playerName=John", johnClient.flow) ~> gameService.websocketRoute ~> check {
+      johnClient.expectMessage("[{\"name\":\"John\",\"position\":{\"x\":0,\"y\":0}}]")
+    }
+    WS(s"/?playerName=Andrew", andrewClient.flow) ~> gameService.websocketRoute ~> check {
+      andrewClient.expectMessage("[{\"name\":\"John\",\"position\":{\"x\":0,\"y\":0}},{\"name\":\"Andrew\",\"position\":{\"x\":0,\"y\":1}}]")
+    }
+  }
+
+
+  def assertWebsocket(playerName: String)(assertions:(WSProbe) => Unit) : Unit = {
     val gameService = new GameService()
     val wsClient = WSProbe()
-
-    // WS creates a WebSocket request for testing
-    WS("/", wsClient.flow) ~> gameService.websocketRoute ~>
-      check {
-        wsClient.expectMessage("Welcome to Pokemon-go player!")
-      }
-
+    WS(s"/?playerName=$playerName", wsClient.flow) ~> gameService.websocketRoute ~> check(assertions(wsClient))
   }
 }
-
-class GameService() extends Directives{
-
-  implicit var actorsystem = ActorSystem()
-  implicit var actorMaterializer = ActorMaterializer()
-
-
-def greeter: Flow[Message, Message, Any] = Flow.fromGraph(GraphDSL.create(){ implicit builder =>
-  import GraphDSL.Implicits._
-
-  val materialization = builder.materializedValue.map(m => TextMessage("Welcome to Pokemon-go player!"))
-  val messagePassingFlow = builder.add(Flow[Message].map(m => m))
-  val merge1 = builder.add(Merge[Message](2))
-  materialization ~> merge1.in(0)
-  merge1 ~> messagePassingFlow
-  FlowShape(merge1.in(1),messagePassingFlow.out)
-
-})
-  val websocketRoute = get {
-    handleWebSocketMessages(greeter)
-  }
-
-}
-
