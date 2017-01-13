b355619d4783269cdbeef971836d5ea18eec1350
tux3 <barrdetwix@gmail.com>
2017-01-13 23:51:05 +0100

    Update server test default position

--- poca/server/src/test/scala/ServerTest.scala
+++ poca/server/src/test/scala/ServerTest.scala
@@ -16,7 +16,7 @@ class ServerTest extends FunSuite with Matchers with ScalatestRouteTest {
 
   test("should register player") {
     assertWebsocket("John"){ wsClient =>
-      wsClient.expectMessage("[{\"name\":\"John\",\"position\":{\"x\":300.0,\"y\":300.0}}]")
+      wsClient.expectMessage("[{\"name\":\"John\",\"position\":{\"x\":0.0,\"y\":0.0}}]")
     }
   }
 
@@ -26,10 +26,10 @@ class ServerTest extends FunSuite with Matchers with ScalatestRouteTest {
     val andrewClient = WSProbe()
 
     WS(s"/?playerName=John", johnClient.flow) ~> gameService.websocketRoute ~> check {
-      johnClient.expectMessage("[{\"name\":\"John\",\"position\":{\"x\":300.0,\"y\":300.0}}]")
+      johnClient.expectMessage("[{\"name\":\"John\",\"position\":{\"x\":0.0,\"y\":0.0}}]")
     }
     WS(s"/?playerName=Andrew", andrewClient.flow) ~> gameService.websocketRoute ~> check {
-      andrewClient.expectMessage("[{\"name\":\"John\",\"position\":{\"x\":300.0,\"y\":300.0}},{\"name\":\"Andrew\",\"position\":{\"x\":300.0,\"y\":300.0}}]")
+      andrewClient.expectMessage("[{\"name\":\"John\",\"position\":{\"x\":0.0,\"y\":0.0}},{\"name\":\"Andrew\",\"position\":{\"x\":0.0,\"y\":0.0}}]")
     }
   }
 
